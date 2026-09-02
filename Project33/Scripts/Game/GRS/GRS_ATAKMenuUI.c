5c1792ae5185aedd4d3ec11ee4fea311349c65f7
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-02 09:37:27 -0400

    Rename ATAK menu UI prefix from GRS to RHD

--- Project33/Scripts/Game/GRS/GRS_ATAKMenuUI.c
+++ Project33/Scripts/Game/GRS/GRS_ATAKMenuUI.c
@@ -1,61 +1,39 @@
-class GRS_ATAKMenuUI : ChimeraMenuBase
+class RHD_ATAKMenuUI : ChimeraMenuBase
 {
-	protected static const string BUTTON_CLOSE = "GRS_ATK_Close";
-	protected static const string TEXT_MONEY = "GRS_ATK_Money";
-	protected static const string TEXT_CONTENT = "GRS_ATK_Content";
-
-	protected ref GRS_ATAKMapState m_MapState;
-
+	protected static const string BUTTON_CLOSE = "RHD_ATK_Close";
+	protected static const string TEXT_MONEY = "RHD_ATK_Money";
+	protected static const string TEXT_CONTENT = "RHD_ATK_Content";
+	protected ref RHD_ATAKMapState m_MapState;
 	override void OnMenuOpen()
 	{
 		super.OnMenuOpen();
-		m_MapState = new GRS_ATAKMapState();
-
+		m_MapState = new RHD_ATAKMapState();
 		Widget root = GetRootWidget();
-		if (!root)
-			return;
-
+		if (!root) return;
 		SCR_ButtonTextComponent closeButton = SCR_ButtonTextComponent.GetButtonText(BUTTON_CLOSE, root);
-		if (closeButton)
-			closeButton.m_OnClicked.Insert(OnCloseClicked);
-
+		if (closeButton) closeButton.m_OnClicked.Insert(OnCloseClicked);
 		RefreshStats();
-
 		InputManager inputManager = GetGame().GetInputManager();
-		if (inputManager)
-			inputManager.AddActionListener("MenuBack", EActionTrigger.DOWN, CloseMenu);
+		if (inputManager) inputManager.AddActionListener("MenuBack", EActionTrigger.DOWN, CloseMenu);
 	}
-
 	override void OnMenuClose()
 	{
 		InputManager inputManager = GetGame().GetInputManager();
-		if (inputManager)
-			inputManager.RemoveActionListener("MenuBack", EActionTrigger.DOWN, CloseMenu);
+		if (inputManager) inputManager.RemoveActionListener("MenuBack", EActionTrigger.DOWN, CloseMenu);
 		super.OnMenuClose();
 	}
-
-	protected void OnCloseClicked()
-	{
-		CloseMenu();
-	}
-
+	protected void OnCloseClicked() { CloseMenu(); }
 	protected void CloseMenu(float value = 0, EActionTrigger reason = EActionTrigger.DOWN)
 	{
-		GetGame().GetMenuManager().CloseMenuByPreset(ChimeraMenuPreset.GRS_ATAK_Menu);
+		GetGame().GetMenuManager().CloseMenuByPreset(ChimeraMenuPreset.RHD_ATAK_Menu);
 	}
-
 	void RefreshStats()
 	{
 		Widget root = GetRootWidget();
-		if (!root)
-			return;
-
+		if (!root) return;
 		TextWidget money = TextWidget.Cast(root.FindAnyWidget(TEXT_MONEY));
-		if (money)
-			money.SetText("$0");
-
+		if (money) money.SetText("$0");
 		TextWidget content = TextWidget.Cast(root.FindAnyWidget(TEXT_CONTENT));
-		if (content)
-			content.SetText("Shop | Casino | Map | Stats | Handler");
+		if (content) content.SetText("Shop | Casino | Map | Stats | Handler");
 	}
 };
