48319f38272425cc7fc810b58d48b3779dc4ee65
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-02 09:36:45 -0400

    Rename game mode GRS references to RHD

--- Project33/Scripts/Game/GRS/GRS_ATAKGameMode.c
+++ Project33/Scripts/Game/GRS/GRS_ATAKGameMode.c
@@ -1,38 +1,31 @@
 modded class SCR_BaseGameMode
 {
-	protected ref GRS_ATAKMenuController m_GRS_ATAKMenu;
-	protected ref GRS_AdminController m_GRS_AdminMenu;
-	protected ref GRS_VirtualPlayerController m_GRS_VirtualPlayerMenu;
+	protected ref RHD_ATAKMenuController m_RHD_ATAKMenu;
+	protected ref RHD_AdminController m_RHD_AdminMenu;
+	protected ref RHD_VirtualPlayerController m_RHD_VirtualPlayerMenu;
 
 	override void OnGameStart()
 	{
 		super.OnGameStart();
-
-		// F6 is standalone and never checks for an ATAK inventory item.
-		m_GRS_ATAKMenu = new GRS_ATAKMenuController();
-		m_GRS_ATAKMenu.Initialize();
-
-		m_GRS_AdminMenu = GRS_AdminMenuService.GetInstance();
-		m_GRS_AdminMenu.Initialize();
-
-		m_GRS_VirtualPlayerMenu = GRS_VirtualPlayerMenuService.GetInstance();
-		m_GRS_VirtualPlayerMenu.Initialize();
+		m_RHD_ATAKMenu = new RHD_ATAKMenuController();
+		m_RHD_ATAKMenu.Initialize();
+		m_RHD_AdminMenu = RHD_AdminMenuService.GetInstance();
+		m_RHD_AdminMenu.Initialize();
+		m_RHD_VirtualPlayerMenu = RHD_VirtualPlayerMenuService.GetInstance();
+		m_RHD_VirtualPlayerMenu.Initialize();
 	}
 
 	override void OnGameEnd()
 	{
-		if (m_GRS_ATAKMenu)
-			m_GRS_ATAKMenu.Shutdown();
-		m_GRS_ATAKMenu = null;
-
-		if (m_GRS_AdminMenu)
-			m_GRS_AdminMenu.Shutdown();
-		m_GRS_AdminMenu = null;
-
-		if (m_GRS_VirtualPlayerMenu)
-			m_GRS_VirtualPlayerMenu.Shutdown();
-		m_GRS_VirtualPlayerMenu = null;
-
+		if (m_RHD_ATAKMenu)
+			m_RHD_ATAKMenu.Shutdown();
+		m_RHD_ATAKMenu = null;
+		if (m_RHD_AdminMenu)
+			m_RHD_AdminMenu.Shutdown();
+		m_RHD_AdminMenu = null;
+		if (m_RHD_VirtualPlayerMenu)
+			m_RHD_VirtualPlayerMenu.Shutdown();
+		m_RHD_VirtualPlayerMenu = null;
 		super.OnGameEnd();
 	}
 };
