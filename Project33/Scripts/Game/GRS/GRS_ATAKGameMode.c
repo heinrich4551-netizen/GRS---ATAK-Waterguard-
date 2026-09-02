5dada7a2a66b6b9608ef139d35224182c7035ba7
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-02 08:46:56 -0400

    Initialize F8 virtual player menu

--- Project33/Scripts/Game/GRS/GRS_ATAKGameMode.c
+++ Project33/Scripts/Game/GRS/GRS_ATAKGameMode.c
@@ -2,18 +2,21 @@ modded class SCR_BaseGameMode
 {
 	protected ref GRS_ATAKMenuController m_GRS_ATAKMenu;
 	protected ref GRS_AdminController m_GRS_AdminMenu;
+	protected ref GRS_VirtualPlayerController m_GRS_VirtualPlayerMenu;
 
 	override void OnGameStart()
 	{
 		super.OnGameStart();
 
-		// F6 is a standalone menu. It never checks for an ATAK inventory item.
+		// F6 is standalone and never checks for an ATAK inventory item.
 		m_GRS_ATAKMenu = new GRS_ATAKMenuController();
 		m_GRS_ATAKMenu.Initialize();
 
-		// F7 uses the same controller instance as the admin UI service.
 		m_GRS_AdminMenu = GRS_AdminMenuService.GetInstance();
 		m_GRS_AdminMenu.Initialize();
+
+		m_GRS_VirtualPlayerMenu = GRS_VirtualPlayerMenuService.GetInstance();
+		m_GRS_VirtualPlayerMenu.Initialize();
 	}
 
 	override void OnGameEnd()
@@ -25,6 +28,10 @@ modded class SCR_BaseGameMode
 		if (m_GRS_AdminMenu)
 			m_GRS_AdminMenu.Shutdown();
 		m_GRS_AdminMenu = null;
+
+		if (m_GRS_VirtualPlayerMenu)
+			m_GRS_VirtualPlayerMenu.Shutdown();
+		m_GRS_VirtualPlayerMenu = null;
 
 		super.OnGameEnd();
 	}
