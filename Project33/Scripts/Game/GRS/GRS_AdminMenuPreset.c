7cb9bbbbcb01cc1f6c5ed0aa791b798cd7fe6c57
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-02 08:18:35 -0400

    Register F7 admin menu preset

---
+++ Project33/Scripts/Game/GRS/GRS_AdminMenuPreset.c
@@ -0,0 +1,6 @@
+// Registers the F7 admin menu with Reforger's global menu preset enum.
+// The matching preset/layout entry must be created in Workbench.
+modded enum ChimeraMenuPreset : ScriptMenuPresetEnum
+{
+	GRS_Admin_Menu
+};
