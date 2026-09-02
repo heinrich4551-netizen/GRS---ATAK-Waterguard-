739d96ac105b8a22055168014b2e8060c5e70fe0
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-02 07:49:33 -0400

    Register GRS ATAK menu preset

---
+++ Project33/Scripts/Game/GRS/GRS_ATAKMenuPreset.c
@@ -0,0 +1,6 @@
+// Registers the custom menu name with Reforger's global menu preset enum.
+// The matching entry must also be added to the mod's chimeraMenus.conf through Workbench.
+modded enum ChimeraMenuPreset : ScriptMenuPresetEnum
+{
+	GRS_ATAK_Menu
+};
