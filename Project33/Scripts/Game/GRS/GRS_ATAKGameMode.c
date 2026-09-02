927b0f0a75a2b3dc4a46a2563c2e5e4530f69791
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-02 08:06:03 -0400

    Make F6 menu independent of ATAK inventory items

--- Project33/Scripts/Game/GRS/GRS_ATAKGameMode.c
+++ Project33/Scripts/Game/GRS/GRS_ATAKGameMode.c
@@ -6,6 +6,8 @@ modded class SCR_BaseGameMode
 	{
 		super.OnGameStart();
 
+		// The menu is a standalone UI feature. It does not require an ATAK item,
+		// inventory object, or any other ATAK-named asset to exist.
 		m_GRS_ATAKMenu = new GRS_ATAKMenuController();
 		m_GRS_ATAKMenu.Initialize();
 	}
