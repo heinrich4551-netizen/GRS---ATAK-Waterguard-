cdca47b38b53f7d3dee5fc1ecfd62bd3fe921811
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-02 08:45:35 -0400

    Add virtual player system configuration

---
+++ Project33/Scripts/Game/GRS/GRS_VirtualPlayerConfig.c
@@ -0,0 +1,14 @@
+class GRS_VirtualPlayerConfig
+{
+	static const int MAX_BASE_INVENTORY_ITEMS = 120;
+	static const int MAX_VIRTUAL_ITEMS = 435;
+	static const int MAX_GARAGE_VEHICLES = 50;
+	static const int MAX_PROPERTIES = 10;
+	static const int MAX_PROPERTY_STORAGE = 15000;
+	static const int STORAGE_PER_OBJECT = 100;
+	static const float MAX_BUILD_RADIUS_FT = 500.0;
+	static const float MAX_BUILD_RADIUS_M = 152.4;
+	static const int MAX_PROPERTY_PRICE = 15000000;
+	static const int MIN_PROPERTY_PRICE = 1;
+	static const float BUILDING_CLUSTER_RADIUS_M = 152.4;
+};
