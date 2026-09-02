4440c4190eccd3bde5feeccf04927767244826ca
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-02 09:38:38 -0400

    Rename shared shop config prefix from GRS to RHD

--- Project33/Scripts/Game/GRS/GRS_ShopConfig.c
+++ Project33/Scripts/Game/GRS/GRS_ShopConfig.c
@@ -1,5 +1,5 @@
-// GRS SHOP - EASY CONFIG
-class GRS_ShopConfig
+// RHD SHOP - EASY CONFIG
+class RHD_ShopConfig
 {
 	// One shop service is shared by F6, F7 and F8.
 	// Prices are deliberately kept in one file for easy editing.
@@ -9,18 +9,15 @@ class GRS_ShopConfig
 	static const int CORN_COB_SELL = 20;
 	static const int GRAPES_SELL = 30;
 	static const int PEACHES_SELL = 30;
-
 	static const int IRON_ORE_SELL = 40;
 	static const int COPPER_ORE_SELL = 55;
 	static const int GOLD_ORE_SELL = 250;
 	static const int DIAMOND_SELL = 1000;
 	static const int OIL_SAND_SELL = 80;
-
 	static const int IRON_SELL = 100;
 	static const int COPPER_SELL = 125;
 	static const int GOLD_SELL = 500;
 	static const int OIL_SELL = 175;
-
 	static const int CANNABIS_FLOWER_SELL = 15;
 	static const int COCAINE_SELL = 150;
 	static const int CANNED_CORN_SELL = 75;
